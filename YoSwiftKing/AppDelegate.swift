48980143983bd6a13759b9324c6c6ae558d340b2
YKK <738816656@qq.com>
2022-06-13 10:34:13 +0800

    update

--- YoSwiftKing/AppDelegate.swift
+++ YoSwiftKing/AppDelegate.swift
@@ -7,34 +7,20 @@
 
 import UIKit
 import SwiftyJSON
+import DefaultsKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    lazy  var tabBarCtl = YoTabBarCtl()
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        rootViewCtlAction()
         accountLogin("hanguiguang", password: "123456")
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-    @available(iOS 13.0, *)
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-    @available(iOS 13.0, *)
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
 
 }
-
-
-
-
 
 /*
 Swift 基础语法+基础理论
@@ -57,6 +43,27 @@ Swift 基础语法+基础理论
  //https://www.jianshu.com/p/905ba2a85455
  
  */
+
+
+
+extension AppDelegate {
+    func  rootViewCtlAction(){
+        
+//        window?.rootViewController  = self.tabBarCtl
+//
+//        return
+        let defaults = Defaults()
+        if defaults.has(.token) {
+            if    let token =  defaults.get(for: .token), token.count > 0  {
+                debugPrint(token)
+                window?.rootViewController  = self.tabBarCtl
+                return
+            }
+        }
+        self.tabBarCtl = YoTabBarCtl()
+        window?.rootViewController  = UINavigationController.init(rootViewController: LoginViewCtl())
+    }
+}
 
 extension AppDelegate {
 
