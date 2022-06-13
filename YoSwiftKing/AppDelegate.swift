e74f63450d1c61dfa72848597e742ba4571a1c2e
YKK <738816656@qq.com>
2022-06-13 10:06:31 +0800

    删除 main  sb

--- YoSwiftKing/AppDelegate.swift
+++ YoSwiftKing/AppDelegate.swift
@@ -13,7 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     lazy  var tabBarCtl = YoTabBarCtl()
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
         rootViewCtlAction()
         accountLogin("hanguiguang", password: "123456")
         return true
